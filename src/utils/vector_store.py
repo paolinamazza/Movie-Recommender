1a47dcbe1f6f7b7a2b61025fad81147b4214d8ca
Giorgio Mazza <giorgiomazza27@ComputeiGiorgio.homenet.telecomitalia.it>
2025-11-25 22:26:49 +0100

    Fix: Add missing collection.add() call in vector_store.py index_content method

--- src/utils/vector_store.py
+++ src/utils/vector_store.py
@@ -229,6 +229,12 @@ class VectorStoreManager:
             documents = [self.create_document_text(item) for item in batch]
             metadatas = [self.create_metadata(item) for item in batch]
             
+            # Add batch to collection
+            self.collection.add(
+                ids=ids,
+                documents=documents,
+                metadatas=metadatas
+            )
 
             logger.info(f"Indexed batch {i // batch_size + 1}/{(len(content_data) - 1) // batch_size + 1}")
 
